c8d080df519e03b5e981c8c0a11126d4f01b8ef4
bhargav-automation <mbhargav013@gmail.com>
2020-08-12 13:21:22 +0530

    F&B project

---
+++ src/Customer_Application/keyword.java
@@ -0,0 +1,104 @@
+package Customer_Application;
+
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class keyword {
+
+	static WebDriver driver;
+
+	public void url(String url) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "Driver//chromedriver.exe");
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		Thread.sleep(500);
+		driver.get(url);
+	}
+
+	public void dropdown(String path, String value) {
+		waitForElement(path);
+		WebElement ops = driver.findElement(By.xpath(path));
+		Select sel = new Select(ops);
+		sel.selectByVisibleText(value);
+	}
+
+	public void Sendkeys(String path, String value) throws InterruptedException {
+		Clickaction(path);
+		waitForElement(path);
+		driver.findElement(By.xpath(path)).sendKeys(value);
+
+	}
+
+	public void Sendkeys1(String path, String value) throws InterruptedException {
+//		Clickaction(path);
+//		waitForElement(path);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.findElement(By.xpath(path)).sendKeys(value);
+
+	}
+
+	public void Clickaction(String path) throws InterruptedException {
+
+		waitForElement(path);
+		driver.findElement(By.xpath(path)).click();
+
+	}
+
+	public void ScrollUp(String path) throws InterruptedException {
+		WebElement element = driver.findElement(By.xpath(path));
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+
+	}
+
+	public void canvasESign(String element) {
+		WebElement target = waitForElement(element);
+		target.click();
+		System.out.println(target);
+
+		// wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("canves-block"))).click();
+		Actions builder = new Actions(driver);
+		Action drawAction = builder.moveToElement(target, 20, 20) // start points x axis and y axis.
+				.clickAndHold(target).moveByOffset(80, 80) // 2nd points (x1,y1)
+				.dragAndDropBy(target, 80, 80).moveByOffset(150, 20) // 3rd points (x2,y2)
+				.dragAndDropBy(target, 50, 20).moveByOffset(250, 20) // 3rd points (x2,y2)
+				.dragAndDropBy(target, 200, 250).moveByOffset(150, 50) // 3rd points (x2,y2)
+
+				.dragAndDropBy(target, 50, 20).release(target).build();
+		drawAction.perform();
+	}
+    
+	public String GetTex(String path)
+	{
+		waitForElement(path);
+		String Valueterm = driver.findElement(By.xpath(path)).getText();
+//		System.out.println("text "+Valueterm);
+		return Valueterm;
+	}
+	public boolean Displayed(String path)
+	{
+		waitForElement(path);
+		return driver.findElement(By.xpath(path)).isDisplayed();
+	}
+	public static WebElement waitForElement(String item) {
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+//		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(item)));
+		WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(item)));
+		return element;
+	}
+
+}
---
+++ src/Vechile_Inspection/VechileInspection.java
@@ -0,0 +1,118 @@
+package Vechile_Inspection;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.poi.ss.util.NumberToTextConverter;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import Customer_Application.XpathExpressions;
+import Customer_Application.keyword;
+
+public class VechileInspection {
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+
+		File file = new File("Config/Customer.properties");
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(file);
+		prop.load(fis);
+		keyword key = new keyword();
+		key.url(prop.getProperty("VechieInspectionURL"));
+		FileInputStream fis1 = new FileInputStream(prop.getProperty("CustomerFilepath1"));
+
+		XSSFWorkbook wb = new XSSFWorkbook(fis1);
+
+		XSSFSheet sh = wb.getSheet(prop.getProperty("Sheet1"));
+		int rownum = sh.getLastRowNum();
+		String filePath = System.getProperty("user.dir");
+		String ActualResult ="233.57 KB";
+
+		XpathExpressions XP = new XpathExpressions();
+		for (int i = 1; i < 2; i++) {
+			/* General Information */
+			key.dropdown(XP.storeNumber, sh.getRow(i).getCell(0).toString());
+			key.Sendkeys(XP.InspectSubmitBy, sh.getRow(i).getCell(1).toString());
+			key.Sendkeys(XP.customerID, NumberToTextConverter.toText(sh.getRow(i).getCell(2).getNumericCellValue()));
+			key.Sendkeys(XP.AppID, NumberToTextConverter.toText(sh.getRow(i).getCell(3).getNumericCellValue()));
+			key.Sendkeys(XP.firstname, sh.getRow(i).getCell(4).toString());
+			key.Sendkeys(XP.lastname, sh.getRow(i).getCell(5).toString());
+			key.Clickaction(XP.CommonButton);
+
+			/* Vechile Inspection */
+			Thread.sleep(1000);
+			key.dropdown(XP.Year, NumberToTextConverter.toText(sh.getRow(i).getCell(6).getNumericCellValue()));
+			key.Sendkeys(XP.Make, sh.getRow(i).getCell(7).toString());
+			key.Sendkeys(XP.Model, sh.getRow(i).getCell(8).toString());
+			key.Sendkeys(XP.color, sh.getRow(i).getCell(9).toString());
+			key.Sendkeys(XP.Mileage1, NumberToTextConverter.toText(sh.getRow(i).getCell(10).getNumericCellValue()));
+			key.Sendkeys(XP.VIN1, sh.getRow(i).getCell(11).toString());
+			key.Clickaction(XP.CheckboxBtn1);
+			key.Clickaction(XP.CheckboxBtn2);
+			key.Clickaction(XP.CheckboxBtn3);
+			key.Clickaction(XP.CommonButton);
+
+			/* Vehicle Condition */
+			Thread.sleep(1000);
+			key.Clickaction(XP.OverallInterior);
+			key.Clickaction(XP.DoorPanels);
+			key.Clickaction(XP.DashControls);
+			key.Clickaction(XP.Seats);
+			key.Clickaction(XP.CarpetFloorBoard);
+			key.Clickaction(XP.ExteriorCondition);
+			key.Clickaction(XP.Paint);
+			key.Clickaction(XP.HoodTruck);
+			key.Clickaction(XP.QuaterPanels);
+			key.Clickaction(XP.Headlights);
+			key.Clickaction(XP.WindowGlass);
+			key.Clickaction(XP.Tires);
+			key.Clickaction(XP.MechanicalCondition);
+			key.Clickaction(XP.Enginelights);
+			key.Clickaction(XP.BrakeLight);
+			key.Clickaction(XP.OilPressure);
+			key.Clickaction(XP.Ignition);
+			key.Clickaction(XP.GearShifting);
+			key.Clickaction(XP.EngineNoises);
+			key.Clickaction(XP.FluidLeaks);
+			key.Clickaction(XP.CommonButton);
+            
+			/* Picture Upload */
+			key.Sendkeys(XP.GPSID, sh.getRow(i).getCell(12).toString());
+			key.Sendkeys1(XP.DriversideUpload, filePath + sh.getRow(i).getCell(13).toString());
+			key.Sendkeys1(XP.PassengersideUpload, filePath + sh.getRow(i).getCell(14).toString());
+			key.Sendkeys1(XP.FrontsideUpload, filePath + sh.getRow(i).getCell(15).toString());
+			key.Sendkeys1(XP.RearsideUpload, filePath + sh.getRow(i).getCell(16).toString());
+			key.Sendkeys1(XP.VINUpload, filePath + sh.getRow(i).getCell(17).toString());
+			key.Sendkeys1(XP.EnginebayUpload, filePath + sh.getRow(i).getCell(18).toString());
+			key.Sendkeys1(XP.InteriorUpload, filePath + sh.getRow(i).getCell(19).toString());
+			key.Sendkeys1(XP.OdometerUpload, filePath + sh.getRow(i).getCell(20).toString());
+			key.Sendkeys1(XP.GPSIDNumberUpload, filePath + sh.getRow(i).getCell(21).toString());
+			key.Sendkeys(XP.GPSInstallation, sh.getRow(i).getCell(22).toString());
+			
+			for(;;)
+			{
+				try
+				{
+					if((key.GetTex(XP.checkingToSubmit)).equalsIgnoreCase(ActualResult))
+					{
+					    System.out.println(key.GetTex(XP.checkingToSubmit));
+//						key.Clickaction(XP.Submit);
+					    break;
+					}
+				}
+				catch(Exception e)
+				{
+					
+				}
+			}
+
+			
+
+		}
+	}
+
+}
